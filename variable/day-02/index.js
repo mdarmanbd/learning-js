a20fb9c2e2d4be395945e375546030b3f11cf76d
Arman <md.armantext@gmail.com>
2022-10-12 00:31:35 +0600

    attched day-o2

---
+++ variable/day-02/index.js
@@ -0,0 +1,96 @@
+
+//valid variable
+
+/*
+let person = "zonayed ahmed";
+let some_thing = "Anything";
+let _canI = "yes";
+let $text = "i am valid text";
+
+console.log (person);
+console.log (some_thing);
+console.log (_canI);
+console.log ($text);
+*/
+
+//invalid variable 
+
+/*
+let !name = "name";
+let @bro = "saikd";
+let #bro = "kdikd";
+
+console.log(!name);
+console.log (@bro);
+console.log (#bro);
+
+
+
+let my name = "askar";
+console.log (my name);
+
+let my_number = "askar";
+console.log (my_number);
+
+let myName = "abu rayhan";
+console.log (myName);
+*/
+
+/*
+let myName = "abu rayhan";
+let author = myName;
+
+console.log (author);
+*/
+
+// data type
+
+/*
+let aNamber = 10;
+let anotherNamber = 12.34;
+
+console.log(aNamber);
+console.log (anotherNamber);
+
+let text = "i want to say samthing";
+console.log(text);
+
+let text2 = "this exctly the same way , but use either one";
+console.log (text2);
+
+let isString = "12";
+console.log ("this is string :" + isString);
+
+let isTrue = abcf; // not a number or string;
+console.log(isTrue);
+
+let isUndefiend = notfound; // undefiend
+console.log(isUndefiend);
+
+let something = ; // undefiend ;
+console.log (something);
+
+let someThing = null ; // null;
+console.log (someThing);
+
+*/
+
+//concatenation 
+
+/*
+let text = "how are " + "you";
+console.log (text);
+
+let welComeMsg = "hello " + text ;
+console.log (welComeMsg); // typeof(welComeMsg);
+
+let number = 10 +10 ;
+console.log(number); //typeof(number);
+
+let isString = 10 + " hello world"; // typeof (isString);
+console.log (isString);
+
+let complex = 10 + 10 + " i am complex " + 30 ; //typeof (complex);
+console.log(complex);
+
+*/
