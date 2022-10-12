56cc87875cfe7a87bda0f097a94f8910dd7d480a
Arman <md.armantext@gmail.com>
2022-10-13 00:20:09 +0600

    attched day-03

---
+++ variable/day-03/index.js
@@ -0,0 +1,73 @@
+
+
+
+// variable change;
+
+
+let myName = 'abu sufiyan-sawrry';
+ 
+myName = 'abu rased al-din';
+console.log(myName);
+
+myName = 'hayder ali';
+console.log(myName);
+
+console.log(typeof(myName));
+
+myName = 20;
+console.log(typeof(myName));
+
+myName = 'abu hena rony';
+console.log(typeof(myName));
+
+
+// operator
+
+let num1 = 10;
+let num2 = 20;
+
+console.log(num1 + num2); // add arithmentic operator; 
+console.log(num2 - num1); // subdtract arithmetic operator;
+console.log(num1 * num2); // multi arithmetic operator;
+console.log(num2 / num1); // divided arithmetic operator;
+console.log(num1 % num2);
+
+/*
+num1++;
+console.log(num1);
+
+num2--;
+console.log(num2);
+*/
+
+console.log(++num1);
+console.log(--num2);
+
+
+let x = 6;
+let y = 7;
+
+console.log(x==y); // both side value equal or not.
+
+let a = 'said';
+let b = 'said';
+
+console.log(a==b); // both side value equal or not.
+
+let p = 4;
+let q = 4;
+
+console.log(p!=q); // not equal.
+
+console.log(num1>num2); // false.
+console.log(num1<num2); // true.
+
+console.log(p<=q); // true.
+console.log(p<q); // false.
+
+let numberA = 20;
+let numberB = '20';
+
+console.log(numberA == numberB); // focuse on valu and true.
+console.log(numberA === numberB); //one is string and one number ,false;
+console.log(p===q); // two are number and same value , true.
