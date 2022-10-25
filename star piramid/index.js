dc90706d03cb9f588845c5d4b7599ea360a80014
Arman <md.armantext@gmail.com>
2022-10-26 00:07:10 +0600

    attached the piramid

---
+++ star piramid/index.js
@@ -0,0 +1,85 @@
+
+
+// for (let r = 1; r<=3 ; r++){
+//     for (let c = 1; c<=r; c++){
+//         document.write (c + ' ');
+//     }
+//     document.write( ' ' + '</br>');
+    
+// }
+
+
+/*
+for (let r = 1 ; r <= 5 ; r++){
+    for (let c = 1; c<=r ; c++){
+        document.write("*");
+    }
+    document.write('<br>');
+  
+}
+
+for (let a = 4 ; a >=1 ; a--){
+    for (let d = 1; d<=a ; d++){
+        document.write("*");
+    }
+    document.write('<br>');
+  
+}
+*/
+
+/*
+for (let a = 1; a<=5; a++) {
+    for (let b = 5; b>=a; b--){
+        document.write('&nbsp');
+    }
+    for(let c = 1; c<=a; c++){
+        document.write('*' + '&nbsp');
+    }
+    document.write('</br>');
+}
+*/
+
+
+/*
+for (let a = 1; a<=5; a++) {
+    for (let b = 5; b>=a; b--){
+        document.write('&nbsp');
+    }
+    for(let c = 1; c<=a; c++){
+        document.write('*' + '&nbsp');
+    }
+    document.write('</br>');
+}
+
+for (let d = 4; d>=1; d--){
+    for (let e = 5; e>=d; e--){
+        document.write('&nbsp');
+    }
+    for (let f = 1; f<=d; f++){
+        document.write( '*' + '&nbsp');
+    }
+    document.write('</br>');
+}
+*/
+
+/*
+for (let a = 1; a<=5; a++){
+   for (let b = 5; b>=a; b--){
+    document.write(' &nbsp');
+   }
+   for (let c = 1; c<=a; c++){
+    document.write('*');
+   }
+    document.write('</br>');
+}
+*/
+
+
+// let a,b;
+
+// for (a=5; a>=1; a--){
+//     for (b=0; b < a; b++){
+//         document.write("*");
+//     }
+//     document.write(" " +"</br>");
+// }
