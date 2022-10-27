489cfa69bca4f93186893d812d3a7a8c5bb6fb0a
Arman <md.armantext@gmail.com>
2022-10-27 19:39:32 +0600

    attached day-02

---
+++ function/day-02/index.js
@@ -0,0 +1,55 @@
+
+/*
+let x = 4;
+let y = 3;
+
+function suttroA (x,y){
+    let a = (x**2 + 2*x*y + y**2);
+    console.log('(a + b) : ' + a);
+    
+}
+suttroA (x,y);
+
+function suttroB (x,y){
+    let a = (x**2 - 2*x*y + y**2);
+    console.log('(a - b) : ' + a);
+    
+}
+suttroB (x,y);
+
+function suttroC (x,y){
+    let a = (x + y) * (x - y);
+    console.log('(a - b) (a + b) : ' + a);
+    
+}
+suttroC (x,y);
+*/
+
+//program to count total number of charchter of string.
+
+/*
+let myName = 'rayhan' 
+
+function number(myName){
+    let x = (myName.length);
+    console.log(x);
+}
+
+number(myName);
+*/
+
+
+
+let x ;
+console.log(typeof(x));
+
+
+
+
+// non primitive. (loop,condition,function);
+
+// function anyName( name){
+//     let x = (name.length);
+//     console.log(x);
+// }
+// anyName( 'vvbvb');
