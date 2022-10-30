70d915742af1b0a8b0363bc1cd80827251a17dd0
Arman <md.armantext@gmail.com>
2022-10-30 17:07:12 +0600

    attached day-04

---
+++ function/day-04/index.js
@@ -0,0 +1,47 @@
+
+/*
+function sentence (longSentence){
+  let sentenceSplit = longSentence.split(" "); // sentenceSplit = 'shh', 'ewr', 'dfois', 'gsf';
+  let longWord = 0;
+  for (let i = 0; i < sentenceSplit.length; i++){ // i = 0; i<4; i++;
+
+    if(sentenceSplit[i].length > longWord){ // 1 > 0 ;
+
+      longWord = sentenceSplit[i].length; // longWord = sentenceSplit[0].length; longWord = 3;
+      
+    }
+    
+  }
+  return longWord;
+}
+
+console.log(sentence('shh ewr dfois gsf'));
+*/
+
+/*
+
+function largestWord(sentence){
+  let newSentence = sentence.split(' ');
+   let bigWord = '';
+ 
+  let word = 0;
+for (let i = 0; i< newSentence.length; i++){
+  if(newSentence[i].length > word){
+    word = newSentence[i].length;
+    bigWord = newSentence[i];
+  
+  }
+  
+}
+
+  console.log(bigWord);
+// console.log(newSentence[3].length);
+}
+
+
+largestWord('this is our country Bangladesh');
+
+*/
+
+
+
