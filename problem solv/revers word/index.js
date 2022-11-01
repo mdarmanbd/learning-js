9448d911a6c687be08137b354adf921d3a3583f8
Arman <md.armantext@gmail.com>
2022-11-02 00:03:57 +0600

    attached reverse word

---
+++ problem solv/revers word/index.js
@@ -0,0 +1,37 @@
+
+/*
+let a = ['word'];
+let b = a[0].split('');
+ for (let i = (b.length -1); i >= 0; i--){
+    let c = b[i];
+   console.log(c);
+ }
+*/
+
+/*
+function newFunction(name){
+    let nam = name.split('');
+    // console.log(nam);
+    for (let i = (nam.length -1); i>=0; i--){
+       let x = nam[i];
+       console.log(x);
+    }
+}
+newFunction('arman');
+*/
+
+/*
+let a = ['hello'] ;
+let b = a[0].split('');
+console.log(b.reverse());
+*/
+
+/*
+function reverseString(str){
+    let splitString = str.split(''); // a r m a n
+    let reverse = splitString.reverse(); // n a m r a
+    let add = reverse.join(''); // join the no space in reverse variable. // namra
+    return add;
+}
+console.log(reverseString('arman'));
+*/
