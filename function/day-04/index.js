fc7c6a2637e4f03bde4a22ba5c2ff73d17f1f089
Arman <md.armantext@gmail.com>
2022-11-02 00:08:19 +0600

    attched day-04

--- function/day-04/index.js
+++ function/day-04/index.js
@@ -44,4 +44,10 @@ largestWord('this is our country Bangladesh');
 */
 
 
+for (let i = 1; i<100; i++){  // i = 1 2 3 4 5
 
+  if( i%2 == 1) { // i = 1 2 3 4 5
+    document.write(i + '</br>'); // i = 1 3 5
+  }
+   
+  }
