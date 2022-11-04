d076a0d52162642bcc51967e3434bbc96dfcf9c7
Arman <md.armantext@gmail.com>
2022-11-04 15:43:19 +0600

    attched arrows function

---
+++ function/arrows function,href,assign,replace/index.js
@@ -0,0 +1,54 @@
+
+
+/*
+let newFunction = (name) => {
+    console.log(name);
+}
+newFunction('arman');
+*/
+
+/*
+let resumeOf = (name,location) => 
+    name + ' hossain' +
+    location + ' uttara' 
+    
+console.log(resumeOf('arman',' dhaka'));
+*/
+
+/*
+let myFunction = 
+
+name => 
+location => 
+name + ' hossain' +
+location + ' uttara'
+
+console.log(myFunction('arman') (' dhaka'));
+*/
+
+/*
+function myFunction(a){ // a = 2;
+    return(b) =>{ // a = 2, b = 3
+        return(c) =>{ // a =2, b = 3, c = 4;
+            return (a+b+c); // 2+3+4;
+        }
+    }
+}
+console.log(myFunction(2)(3)(4));
+*/
+
+// function myButton(){
+//     location.href = 'https://translate.google.com/'; // back arrow is work;
+// }
+
+/*
+function myButton(){
+    location.assign('https://translate.google.com/'); // back arrow is work;
+}
+*/
+
+/*
+function myButton(){
+    location.replace('https://translate.google.com/'); // back arroe is not work;
+}
+*/
