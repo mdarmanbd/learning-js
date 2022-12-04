b63b60f7c64576886355c6225fabf588ed7dadaa
Arman <md.armantext@gmail.com>
2022-12-04 23:37:41 +0600

    attched the date-object

---
+++ date-object/index.js
@@ -0,0 +1,27 @@
+
+// 
+
+
+let newDate = new Date(); // date object , all date are included here.
+console.log(newDate); 
+
+let year = newDate.getFullYear();
+console.log(year);
+
+let month = newDate.getMonth();
+console.log(month); // month are start 0 t0 11, 0 is juneary, 11 is december;
+
+let date = newDate.getDate();
+console.log(date); 
+
+let day = newDate.getDay();
+console.log(day); // day also start is 0 to 6;
+
+let hours = newDate.getHours();
+console.log(hours);
+
+let min = newDate.getMinutes();
+console.log(min);
+
+let sec = newDate.getSeconds();
+console.log(sec);
