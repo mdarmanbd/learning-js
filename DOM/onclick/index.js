5647d4f9729dc82192219553a54ce6aa620ad865
Arman <md.armantext@gmail.com>
2022-12-05 23:27:21 +0600

    attched onclick

---
+++ DOM/onclick/index.js
@@ -0,0 +1,11 @@
+
+
+    let picture = document.querySelector('#newImg');
+
+    function function1(){
+        picture.src = 'img/pic1.jpg';
+    }
+
+    function function2(){
+        picture.src ='img/pic2.jpg';
+    }
