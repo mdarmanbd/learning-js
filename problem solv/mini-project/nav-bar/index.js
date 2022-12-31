ddeb69c07985a27aba67a4e9933bafc6659e11ef
Arman <md.armantext@gmail.com>
2022-12-31 22:10:37 +0600

    attched nav

---
+++ problem solv/mini-project/nav-bar/index.js
@@ -0,0 +1,17 @@
+
+
+const mobileNav = document.querySelector('.mobile-nav');
+const button = document.querySelector('.btn');
+
+button.addEventListener('click',function(){
+
+//     if(mobileNav.classList.contains('display')){
+//         mobileNav.classList.remove('display');
+//     }
+
+//    else{
+//         mobileNav.classList.add('display');
+//    }
+   mobileNav.classList.toggle('display');
+
+});
