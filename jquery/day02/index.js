99baa47d592b92d289179a831124acb6c743d3f5
Arman <md.armantext@gmail.com>
2023-01-11 15:00:42 +0600

    attched the day02

---
+++ jquery/day02/index.js
@@ -0,0 +1,24 @@
+
+/*
+// let attr = document.querySelector('a').getAttribute('href');
+// console.log(attr);
+
+let myAttr = $('.google').attr('href');  // .attr = get attribute.
+// console.log(myAttr);
+
+let newAttr = $('.new').removeAttr('href'); // remove attr.
+// console.log(newAttr);
+
+let setAttr = $('.google').attr('href','https://www.bdjobs.com/'); // set attribute.
+console.log(setAttr);
+
+*/
+
+$('#secondId h2').css('color','green'); // single css style add.
+$('#secondId h2').css('font-size','45px'); // single css style add.
+$('#secondId h3').css({'color': 'red', 'font-size': '30px' , 'background': 'grey'}); // multipul css style add.
+
+$('#secondId p').addClass('myStyle'); // single class add
+$('#secondId p').addClass('myStyle myStyle2'); // multipul class add
+
+$('.party').attr('id', 'Idthree'); // add id on a tag.
