25743575669066c430435d01640d68156110bb86
Arman <md.armantext@gmail.com>
2023-01-12 23:36:02 +0600

    attched day04

---
+++ jquery/day04/index.js
@@ -0,0 +1,33 @@
+
+/*
+$('.btn').click(function(){
+   $('.myColor').show(1000); // show 1000sec;
+})
+*/
+/*
+$('.btn').click(function(){
+    $('.myColor').hide(1000); // hide 1000sec;
+ })
+ */
+/*
+ $('.btn').click(function(){
+    $('.myColor').toggle(); // hide & show at a time 1000sec;
+ });
+*/
+/*
+$('.btn').click(function(){
+    $('.myColor').fadeToggle(1000); // fadeIn, fadeOut
+ });
+ */
+
+ /*
+ $('.btn').click(function(){
+    $('.myColor').fadeTo(1000 , 0.5); // fadeIn, fadeOut
+ });
+ */
+
+ $('.btn').click(function(){
+    $('.myColor').slideToggle(); // slideUp, slideDowe
+ });
+
+
